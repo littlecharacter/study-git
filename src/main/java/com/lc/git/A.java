9b6e57e99c7d3c83685e45a03f169866df61b1d3
gujixian <gujixian@meituan.com>
2021-06-01 19:34:42 +0800

    测试merge master

--- src/main/java/com/lc/git/A.java
+++ src/main/java/com/lc/git/A.java
@@ -2,7 +2,7 @@ package com.lc.git;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
         System.out.println("Hello World");
     }
 }
