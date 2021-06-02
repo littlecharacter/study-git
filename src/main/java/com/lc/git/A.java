6ad5b9d76dab54e0e20f59f17de1a6371fdc767d
gujixian <gujixian@meituan.com>
2021-06-02 12:00:35 +0800

    测试reset master

--- src/main/java/com/lc/git/A.java
+++ src/main/java/com/lc/git/A.java
@@ -2,7 +2,7 @@ package com.lc.git;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hello World1");
+        System.out.println("Hello World HAHA");
         System.out.println("Hello World");
     }
 }
